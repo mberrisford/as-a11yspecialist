432fa837d9afa3b83b129df499e122812a7c4c5f
mberrisford <berrisfordm@outlook.com>
2024-11-17 15:24:57 -0500

    added sitemap feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://a11yspecialist.netlify.app"
+    site: "https://a11yspecialist.netlify.app",
+    integrations: [sitemap()],
 });
