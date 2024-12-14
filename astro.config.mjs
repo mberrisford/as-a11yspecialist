8c8a42d2ec6699955c3c9c25e3d982562a695df0
mberrisford <berrisfordm@outlook.com>
2024-12-14 12:30:54 -0500

    updated config to point to new url, contrast report fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://a11yspecialist.netlify.app',
+  site: 'https://accessibilityspecialist.ca',
   integrations: [
     sitemap(),
   ],
